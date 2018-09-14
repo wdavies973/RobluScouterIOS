31c7b6e42363c81f0a691e03cfb1209f5e50a1d4
Will Davies <wdavies973@gmail.com>
2018-09-14 15:39:17 -0500

    Initial Commit

---
+++ RobluScouterIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RobluScouterIOS
+//
+//  Created by Will H Davies on 9/14/18.
+//  Copyright © 2018 Will H Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
