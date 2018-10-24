e7c3b586a340353dfafd87720ca6f6104aafdf79
Will Davies <wdavies973@gmail.com>
2018-10-24 16:12:13 -0500

    Added package file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+//
+//  Package.swift
+//  RobluScouterIOS
+//
+//  Created by Will H Davies on 10/24/18.
+//  Copyright © 2018 Will H Davies. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Greeter",
+
+    dependencies: [
+    .Package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 3, minor: 2),
+    ])
